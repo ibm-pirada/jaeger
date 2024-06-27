a384bce9bad3e36577f45acb56a843f413189546
Pirada Boonna <Pirada.Boonna@ibm.com>
2024-06-28 00:12:41 +0700

    skip some steps

--- scripts/build-upload-a-docker-image.sh
+++ scripts/build-upload-a-docker-image.sh
@@ -7,7 +7,7 @@ docker_file_arg="Dockerfile"
 target_arg=""
 local_test_only='N'
 platforms="linux/amd64"
-namespace="earnpirada"
+namespace="jaegertracing"
 
 while getopts "lbc:d:f:p:t:" opt; do
 	# shellcheck disable=SC2220 # we don't need a *) case
